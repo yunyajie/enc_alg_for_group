c989f5d333e6ba0d0b733a2dc7cf79a4bb6a8985
yunyajie <1545357935@qq.com>
2023-12-02 22:46:00 +0800

    添加了随机生成加密密钥的代码

--- main.cpp
+++ main.cpp
@@ -4,25 +4,16 @@
 #include <vector>
 
 int main(){
-    std::vector<mpz_class> re = {5, 7, 9};
-    std::vector<mpz_class> mod = {7, 11, 23};
-    std::vector<mpz_class> private_keys;
-    std::vector<mpz_class> message = {1, 2, 3, 4, 5, 6, 7, 10, 22, 24}; //加密的明文必须都小于最小的模数7
-    for(int i = 0; i < re.size(); i++){
-        mpz_class pk = get_deckey(re[i], mod[i]);
-        private_keys.push_back(pk);
-        std::cout << "enc_key = " << re[i] << " dec_key = " << pk << std::endl;
+    mpz_class safe_prime;
+    //生成 10 个 1024 位的安全素数
+    for(int i = 0; i < 10; i++){
+        generate_safe_prime(safe_prime, 1024, 25);
+        std::cout << "第 " << i + 1 << " 个 1024 位的安全素数 ：" << safe_prime << std::endl;
     }
-    mpz_class m_key = get_master_key(re, mod);
-    std::cout << "master key = " << m_key << std::endl;
-    mpz_class c;
-    for(int i = 0; i < message.size(); i++){
-        std::cout << "加密消息 m = " << message[i] << ", 密文 c = " << (c = PH_ENC(message[i], m_key, mod)) << std::endl;
-        std::cout << "所有成员解密结果如下：" << std::endl;
-        for(int j = 0; j < private_keys.size(); j++){
-            std::cout << "成员 " << j << " : 解密结果为 m' = " << PH_DEC(c, private_keys[j], mod[j]) << std::endl;
-        }
+    //生成 10 个 2048 位的安全素数
+    for(int i = 0; i < 10; i++){
+        generate_safe_prime(safe_prime, 2048, 25);
+        std::cout << "第 " << i + 1 << " 个 2048 位的安全素数 ：" << safe_prime << std::endl;
     }
-
     return 0;
 }
--- mylib.cpp
+++ mylib.cpp
@@ -1,7 +1,7 @@
 #include "mylib.h"
 
 //CRT算法     返回 0 表示不是一致的，没有解
-mpz_class CRT(std::vector<mpz_class>& remainders, std::vector<mpz_class>& modulus){
+mpz_class CRT(const std::vector<mpz_class>& remainders, const std::vector<mpz_class>& modulus){
     if((remainders.size() != modulus.size()) || remainders.empty()) return 0;
     mpz_class result(0);
     mpz_class m(1);
@@ -25,15 +25,31 @@ mpz_class CRT(std::vector<mpz_class>& remainders, std::vector<mpz_class>& modulu
     return result;
 }
 
+//获取一个随机的解密密钥 其值 < upperLimit，满足条件是 e = 2 * r + 1，即一个奇数，且小于其对应的模数
+mpz_class get_enckey(const mpz_class& modulus, const mpz_class& upperLimit){
+    gmp_randstate_t state;
+    //初始化 GMP 伪随机数生成器状态
+    gmp_randinit_default(state);
+    mpz_class upper = upperLimit - 1;
+    mpz_class randomNum;
+    //生成小于上限的随机数
+    while(true){
+        mpz_urandomm(randomNum.get_mpz_t(), state, upper.get_mpz_t());
+        if(randomNum % 2 != 0 && randomNum != 1){
+            return randomNum;
+        }
+    }
+}
+
 //获取解密密钥
-mpz_class get_deckey(mpz_class& enc_key, mpz_class& modulus){
+mpz_class get_deckey(const mpz_class& enc_key, const mpz_class& modulus){
     mpz_class dec_key;
     mpz_invert(dec_key.get_mpz_t(), enc_key.get_mpz_t(), static_cast<mpz_class>(modulus - 1).get_mpz_t());
     return dec_key;
 }
 
 //获取主加密密钥
-mpz_class get_master_key(std::vector<mpz_class>& enc_keys, std::vector<mpz_class>& modulus){
+mpz_class get_master_key(const std::vector<mpz_class>& enc_keys, const std::vector<mpz_class>& modulus){
     int n = modulus.size();
     mpz_class m(1);
     for(int i = 0; i < n; i++){
@@ -54,7 +70,7 @@ mpz_class get_master_key(std::vector<mpz_class>& enc_keys, std::vector<mpz_class
 }
 
 //PH加密
-mpz_class PH_ENC(mpz_class& message, mpz_class& master_key, std::vector<mpz_class>& members_modulus){
+mpz_class PH_ENC(const mpz_class& message, const mpz_class& master_key, const std::vector<mpz_class>& members_modulus){
     int n = members_modulus.size();
     mpz_class m(1);
     for(int i = 0; i < n; i++){
@@ -66,8 +82,32 @@ mpz_class PH_ENC(mpz_class& message, mpz_class& master_key, std::vector<mpz_clas
 }
 
 //PH解密
-mpz_class PH_DEC(mpz_class& ciphertext, mpz_class& private_key, mpz_class& modulus){
+mpz_class PH_DEC(const mpz_class& ciphertext, const mpz_class& private_key, const mpz_class& modulus){
     mpz_class message;
     mpz_powm(message.get_mpz_t(), ciphertext.get_mpz_t(), private_key.get_mpz_t(), modulus.get_mpz_t());
     return message;
+}
+
+//素性测试 reps 是 Miller_Rabin 素性测试的重复次数
+bool is_prime(const mpz_class& num, const unsigned long int reps){
+    //使用 Miller_Rabin 素性测试进行检查
+    return mpz_probab_prime_p(num.get_mpz_t(), reps);
+}
+
+//生成一个长度为 bit_length 的安全素数
+void generate_safe_prime(mpz_class& safe_prime, const unsigned long int bit_length, const unsigned long int reps){
+    mpz_class prime_candidate;
+    gmp_randstate_t state;
+    //初始化 GMP 伪随机数生成器状态
+    gmp_randinit_default(state);
+    //首先生成一个 bit_length 位的随机奇数
+    mpz_urandomb(prime_candidate.get_mpz_t(), state, bit_length);
+    mpz_setbit(prime_candidate.get_mpz_t(), bit_length - 1); //确保是一个 bit_length 位长的数
+    mpz_setbit(prime_candidate.get_mpz_t(), 0); //确保是一个奇数
+
+    std::cout << "这个随机的奇数是：" << prime_candidate << std::endl;
+    while(!is_prime(prime_candidate, reps) || !is_prime((prime_candidate - 1) / 2, reps)){
+        //不是素数就试探下一个奇数
+        mpz_add_ui(prime_candidate.get_mpz_t(), prime_candidate.get_mpz_t(), 2);
+    }
 }
