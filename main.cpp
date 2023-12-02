4b8b929d4f5395bab5ee1b091abfc6d317013e56
yajie yun <yajieyun@mail.ustc.edu.cn>
2023-12-02 23:48:21 +0800

    将项目放到了实验室电脑上

--- main.cpp
+++ main.cpp
@@ -5,15 +5,16 @@
 
 int main(){
     mpz_class safe_prime;
+    std::cout << "默认的数是：" << safe_prime << std::endl;
     //生成 10 个 1024 位的安全素数
-    for(int i = 0; i < 10; i++){
-        generate_safe_prime(safe_prime, 1024, 25);
-        std::cout << "第 " << i + 1 << " 个 1024 位的安全素数 ：" << safe_prime << std::endl;
-    }
-    //生成 10 个 2048 位的安全素数
-    for(int i = 0; i < 10; i++){
-        generate_safe_prime(safe_prime, 2048, 25);
-        std::cout << "第 " << i + 1 << " 个 2048 位的安全素数 ：" << safe_prime << std::endl;
-    }
+    // for(int i = 0; i < 10; i++){
+    //     generate_safe_prime(safe_prime, 1024, 25);
+    //     std::cout << "第 " << i + 1 << " 个 1024 位的安全素数 ：" << safe_prime << std::endl;
+    // }
+    // //生成 10 个 2048 位的安全素数
+    // for(int i = 0; i < 10; i++){
+    //     generate_safe_prime(safe_prime, 2048, 25);
+    //     std::cout << "第 " << i + 1 << " 个 2048 位的安全素数 ：" << safe_prime << std::endl;
+    // }
     return 0;
 }
