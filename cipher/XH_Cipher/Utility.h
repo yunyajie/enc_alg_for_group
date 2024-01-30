65723b6592afa233fafcd641ea9a558e8c19568a
yajieyun <yajieyun@mail.ustc.edu.cn>
2024-01-30 17:54:27 +0800

    工具类

---
+++ cipher/XH_Cipher/Utility.h
@@ -0,0 +1,13 @@
+#ifndef _UTILITY_H_
+#define _UTILITY_H_
+#include<gmpxx.h>
+#include<string>
+#include<vector>
+#include<openssl/sha.h>
+//工具
+
+//将 strs 中的字符串用 SHA256 做哈希并转换成为 mpz_class 格式存入 result 中
+int sha256encrypt(vector<string>& strs, mpz_class& result);
+
+
+#endif
