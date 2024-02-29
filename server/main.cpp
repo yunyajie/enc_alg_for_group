5b0df0ae539f5c81eb000a5ae99beb44a81ccded
yajie yun <yajieyun@mail.ustc.edu.cn>
2024-02-29 23:16:11 +0800

    实验室测试修改

--- server/main.cpp
+++ server/main.cpp
@@ -2,7 +2,7 @@
 
 
 int main(){
-    Server server(8000, 0, "root", "root", "ph_cipher", 10, 30, 5000);
+    Server server(8000, 0, "root", "@Lab510510", "ph_cipher", 10, 30, 5000);
     server.start();
     return 0;
 }
